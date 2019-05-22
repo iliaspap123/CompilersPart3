8348d5551861929932983c8b29449df072df08b7
iliaspap123 <iliaspap97@gmail.com>
2019-05-22 17:57:16 +0300

    getelemptr

--- first_try/ll_visitor.java
+++ first_try/ll_visitor.java
@@ -2,6 +2,7 @@ import syntaxtree.*;
 import visitor.GJDepthFirst;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
 
 public class ll_visitor extends GJDepthFirst<String[], Map> {
@@ -9,7 +10,7 @@ public class ll_visitor extends GJDepthFirst<String[], Map> {
   int temps_vars;
   int numIfs;
   String currentClass;
-
+  String currentMeth;
 
 
   /**
@@ -30,17 +31,119 @@ public class ll_visitor extends GJDepthFirst<String[], Map> {
   public String[] visit(MethodDeclaration n, Map argu) throws Exception {
      String[] type = n.f1.accept(this, argu);
      String[] meth = n.f2.accept(this, argu);
-     System.out.println("define "+type[1]+" @"+currentClass+"."+meth[1]+"(i8* %this) {");
+     currentMeth = meth[1];
+     String str_def = "define "+type[1]+" @"+currentClass+"."+meth[1]+"(i8* %this";
+     //temp_args = new ArrayList<String[]>();
+     ClassForm classF = (ClassForm) argu.get(currentClass);
+     MethodForm methF = (MethodForm) classF.Methods.get(meth[1]);
+     String str_decl = "";
+     for(String key : methF.Arguments.keySet()) {
+       String type_arg = methF.Arguments.get(key);
+       if(type_arg.equals("int")) {
+         type_arg = "i32";
+       }
+       else if(type_arg.equals("boolean")) {
+         type_arg = "i1";
+       }
+       else {
+         type_arg = "i8*";
+       }
+       str_def += ", "+type_arg+" %."+key;
+       str_decl += "\t%"+key+" = alloca "+type_arg+"\n";
+       str_decl += "\tstore "+type_arg+" %."+key+", "+type_arg+"* %"+key+"\n";
+
+     }
+     System.out.println(str_def+") {");
+     System.out.println(str_decl);
      n.f4.accept(this, argu);
      n.f7.accept(this, argu);
      n.f8.accept(this, argu);
      n.f10.accept(this, argu);
      System.out.println("}");
-
+    // temp_args.clear();
      return null;
   }
 
 
+  /**
+   * f0 -> Type()
+   * f1 -> Identifier()
+   * f2 -> ";"
+   */
+  public String[] visit(VarDeclaration n, Map argu) throws Exception {
+     String[] type = n.f0.accept(this, argu);
+     String[] ident = n.f1.accept(this, argu);
+     // n.f2.accept(this, argu);
+     System.out.println("\t%"+ident[1]+" = alloca "+type[1]);
+     return type;
+  }
+
+  /**
+   * f0 -> IntegerLiteral()
+   *       | TrueLiteral()
+   *       | FalseLiteral()
+   *       | Identifier()
+   *       | ThisExpression()
+   *       | ArrayAllocationExpression()
+   *       | AllocationExpression()
+   *       | BracketExpression()
+   */
+  public String[] visit(PrimaryExpression n, Map argu) throws Exception {
+
+     String pr[] = n.f0.accept(this, argu);
+
+     if(pr!= null && pr[0].equals("Identifier")) {
+
+       String type;
+       ClassForm classF = (ClassForm) argu.get(currentClass);
+       MethodForm methF = classF.Methods.get(currentMeth);
+       // System.out.println("exit1: "+pr[0]+pr[1]);
+       if((type=check_in_meth(pr[1],methF))!=null) {
+         //pr[0] = type2;
+         System.out.println("\t%_"+temps_vars+" = load "+type+", "+type+"* %"+pr[1]);
+         pr[0] = type;
+         pr[1] = "%_"+temps_vars;
+         temps_vars++;
+         // System.out.println("exit2,1: "+type);
+       }
+       else {
+         int offset = check_var(currentClass+"."+pr[1],currentClass,argu);
+         System.out.println("\t%_"+temps_vars+" = getelementptr i8, i8* %this, i32 "+offset);
+         temps_vars++;
+         // System.out.println("exit2,2: "+offset);
+
+       }
+
+     }
+     return pr;
+  }
+
+  /**
+   * f0 -> "("
+   * f1 -> Expression()
+   * f2 -> ")"
+   */
+  public String[] visit(BracketExpression n, Map argu) throws Exception {
+    return n.f1.accept(this, argu);
+  }
+
+  /**
+   * f0 -> PrimaryExpression()
+   * f1 -> "."
+   * f2 -> Identifier()
+   * f3 -> "("
+   * f4 -> ( ExpressionList() )?
+   * f5 -> ")"
+   */
+  public String[] visit(MessageSend n, Map argu) throws Exception {
+     n.f0.accept(this, argu);
+     n.f1.accept(this, argu);
+     n.f2.accept(this, argu);
+     n.f3.accept(this, argu);
+     n.f4.accept(this, argu);
+     n.f5.accept(this, argu);
+     return null;
+  }
 
   /**
   * f0 -> Identifier()
@@ -106,7 +209,10 @@ public class ll_visitor extends GJDepthFirst<String[], Map> {
    */
   public String[] visit(CompareExpression n, Map argu) throws Exception {
 
+    // System.out.println("Compare1");
+
      String[] pr1 = n.f0.accept(this, argu);
+
   //    //String type = null;
      String value1 = pr1[1];
      if(pr1[0].equals("Identifier") ) {
@@ -119,6 +225,7 @@ public class ll_visitor extends GJDepthFirst<String[], Map> {
   //    //   value1 = parts[1];
   //    // }
      String[] pr2 = n.f2.accept(this, argu);
+
      String value2 = pr2[1];
      if(pr2[0].equals("Identifier") ) {
        value2 = "%_"+String.valueOf(temps_vars);
@@ -129,11 +236,13 @@ public class ll_visitor extends GJDepthFirst<String[], Map> {
   //    //   String[] parts = pr2.split(" ");
   //    //   value2 = parts[1];
   //    // }
+
      String[] res= new String[2];
      res[0] = "i1";
      res[1] = "%_"+String.valueOf(temps_vars);
      System.out.println("\t"+res[1]+" = icmp slt i32 "+value1+", "+value2);
      temps_vars++;
+
      return res;
   }
 
@@ -309,6 +418,38 @@ public class ll_visitor extends GJDepthFirst<String[], Map> {
      // n.f3.accept(this, argu);
       n.f4.accept(this, argu);
      return null;
+  }
+
+  String check_in_meth(String var,MethodForm methF) {
+    if(methF.Arguments.containsKey(var)) {
+      return methF.Arguments.get(var);
+    }
+    if(methF.Vars.containsKey(var)) {
+      return methF.Vars.get(var);
+    }
+    return null;
+  }
+
+  int check_var(String var,String className,Map ClassTypes) {
+    /* check if var is definied */
+    ClassForm classF =(ClassForm) ClassTypes.get(className);
+    // System.out.println(var+" here1"+classF.ClassVarInfo.keySet());
+    if(classF.ClassVarInfo.containsKey(var)) { //it definied in currentClass
+      // System.out.println(var+" here2 "+classF.ClassVarInfo.get(var));
+      return classF.ClassVarInfo.get(var)+8;
+    }
+
+    String superClass = classF.Isimpliments;
+    while(superClass != null) { //check in all super classes
+      classF = (ClassForm) ClassTypes.get(superClass);
+
+      if(classF.ClassVars.get(var) != null) {
+        return classF.ClassVarInfo.get(var)+8;
+      }
+
+      superClass = classF.Isimpliments;
+    }
+    return -1;
   }
 
 
